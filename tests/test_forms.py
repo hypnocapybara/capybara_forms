8567d80dfa22e038798a8d4fbb98ccff8ecc115f
Ilpokypop NA <apolevki09@gmail.com>
2019-01-07 18:59:15 +0700

    basic tests added

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from lxml import html
+
+from capybara_forms.models import SelectData
 
 from .utils import create_category
 from .forms import AdvertForm
@@ -9,9 +12,15 @@ class CapybaraFormTestCase(TestCase):
     def setUp(self):
         super(CapybaraFormTestCase, self).setUp()
         self.category = create_category('Cats')
-
-    def test_filter(self):
-        advert = Advert.objects.create(
+        self.breed1 = SelectData.objects.create(
+            key='select.cat.breed',
+            value='Oriental'
+        )
+        self.breed2 = SelectData.objects.create(
+            key='select.cat.breed',
+            value='Bengal'
+        )
+        self.advert1 = Advert.objects.create(
             category=self.category,
             title='advert 1',
             price=100,
@@ -28,9 +37,9 @@ class CapybaraFormTestCase(TestCase):
                 },
                 "breed": {
                     "type": "select",
-                    "value": 1,
+                    "value": self.breed1.pk,
                     "display_name": "Breed",
-                    "display_value": "Oriental"
+                    "display_value": self.breed1.value
                 },
                 "color": {
                     "type": "color",
@@ -53,9 +62,103 @@ class CapybaraFormTestCase(TestCase):
                     "display_name": "is vaccinated"
                 }
             })
+
+        self.advert2 = Advert.objects.create(
+            category=self.category,
+            title='advert 2',
+            price=200,
+            data={
+                "name": {
+                    "type": "string",
+                    "value": "july",
+                    "display_name": "Name"
+                },
+                "year": {
+                    "type": "number",
+                    "value": 2010,
+                    "display_name": "Birth year"
+                },
+                "breed": {
+                    "type": "select",
+                    "value": self.breed2.pk,
+                    "display_name": "Breed",
+                    "display_value": self.breed2.value
+                },
+                "color": {
+                    "type": "color",
+                    "value": "1",
+                    "display_name": "Cat's color"
+                },
+                "height": {
+                    "type": "number",
+                    "value": 10,
+                    "display_name": "Height"
+                },
+                "weight": {
+                    "type": "number",
+                    "value": 5,
+                    "display_name": "Weight"
+                },
+                "vaccinated": {
+                    "type": "checkbox",
+                    "value": True,
+                    "display_name": "is vaccinated"
+                }
+            })
+
+    def test_filter(self):
         form = AdvertForm(self.category)
         adverts = form.filter_adverts()
         self.assertSetEqual(
             set([advert.pk for advert in adverts]),
-            {advert.pk}
+            {self.advert1.pk, self.advert2.pk}
         )
+
+        form = AdvertForm(self.category, data={'price_from': 150, 'price_to': 300})
+        adverts = form.filter_adverts()
+        self.assertSetEqual(
+            set([advert.pk for advert in adverts]),
+            {self.advert2.pk}
+        )
+
+        form = AdvertForm(self.category, data={'data[breed]': self.breed1.pk})
+        adverts = form.filter_adverts()
+        self.assertSetEqual(
+            set([advert.pk for advert in adverts]),
+            {self.advert1.pk}
+        )
+
+        form = AdvertForm(self.category, data={'data[breed]': self.breed1.pk})
+        adverts = form.filter_adverts()
+        self.assertSetEqual(
+            set([advert.pk for advert in adverts]),
+            {self.advert1.pk}
+        )
+
+        form = AdvertForm(self.category, data={'data[year_from]': 2009})
+        adverts = form.filter_adverts()
+        self.assertSetEqual(
+            set([advert.pk for advert in adverts]),
+            {self.advert2.pk}
+        )
+
+    def test_render_form(self):
+        form = AdvertForm(self.category)
+        content = form.render_form()
+        page = html.fragment_fromstring('<div>' + content + '</div>')
+        items_count = len(page.xpath('//div[@class="cpb_form_item"]'))
+        self.assertEqual(items_count, 9)
+
+    def test_render_filter(self):
+        form = AdvertForm(self.category)
+        content = form.render_filter()
+        page = html.fragment_fromstring('<div>' + content + '</div>')
+        items_count = len(page.xpath('//div[@class="cpb_form_item"]'))
+        self.assertEqual(items_count, 10)
+
+    def test_render_item(self):
+        form = AdvertForm(self.category, instance=self.advert1)
+        content = form.render_item()
+        page = html.fragment_fromstring('<div>' + content + '</div>')
+        items_count = len(page.xpath('//div[@class="cpb_field_item"]'))
+        self.assertEqual(items_count, 7)
--- tests/utils.py
+++ tests/utils.py
@@ -1,20 +1,21 @@
 from .models import Category
-from .category_data import cats_category, cats_filter, cats_filter_template
+from .category_data import get_cats_category, \
+    get_cats_filter, get_cats_filter_template
 
 
 def create_category(title, form=None, filter=None,
                     form_template=None, filter_template=None):
     if not form:
-        form = cats_category
+        form = get_cats_category()
 
     if not filter:
-        filter = cats_filter
+        filter = get_cats_filter()
 
     if not form_template:
         form_template = ''
 
     if not filter_template:
-        filter_template = cats_filter_template
+        filter_template = get_cats_filter_template()
 
     return Category.objects.create(
         title=title,
